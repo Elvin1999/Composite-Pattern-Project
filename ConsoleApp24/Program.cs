66852abb54c26fca59e2fbe7dfe073943c58a278
camalzade_elvin <camalzade_elvin@outlook.com>
2018-12-28 20:42:10 +0400

    Done Project

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -66,35 +66,7 @@ namespace ConsoleApp24
             Name = name;
         }
     }
-    class City : IPoll
-    {
-        public string Name { get; set; }
 
-        public void Add(IPoll poll)
-        {
-            polls.Add(poll);
-        }
-
-        public int GetPoll()
-        {
-            int sum = 0;
-            for (int i = 0; i < polls.Count; i++)
-            {
-                sum += polls[i].GetPoll();
-            }
-            return sum;
-        }
-
-        public bool Remove(IPoll poll)
-        {
-            return polls.Remove(poll);
-        }
-        List<IPoll> polls = new List<IPoll>();
-        public City(string name)
-        {
-            Name = name;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -103,12 +75,15 @@ namespace ConsoleApp24
             Agency agency2 = new Agency("Agency1", 25);
             Region region = new Region("Wall st");
             Region region2 = new Region("Wall st2");
-            City city = new City("London");
+            Region city = new Region("London");
+            
             try
             {
+            
                 region.Add(agency); region.Add(agency2);
-                region.Add(agency); region.Add(agency2);
-                city.Add(region); city.Add(region2);
+                city.Add(region);
+                region2.Add(agency); region2.Add(agency2);
+                city.Add(region2);
                 Console.WriteLine($"All polls are {city.GetPoll()} in {city.Name}");
             }
             catch (Exception ex)
